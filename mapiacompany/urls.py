807f8ed95da875ca1c00695508826b43995843c5
TaewooKim <embedded61@gmail.com>
2021-11-06 05:38:36 +0900

    Add: 배포 환경 설정 및 db csv data 추가 - docker 파일 추가 - nginx 설정 추가

--- mapiacompany/urls.py
+++ mapiacompany/urls.py
@@ -14,7 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib          import admin
+from django.conf             import settings
 from django.urls             import path, re_path, include
+from django.conf.urls.static import static
 from drf_yasg                import openapi
 from drf_yasg.views          import get_schema_view
 from strawberry.django.views import GraphQLView
