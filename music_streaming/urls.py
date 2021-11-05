70773540da06f08e8088c630d1a84a945ca1b13d
Jiwon <jiwon@Jiwonui-MacBookAir.local>
2021-11-05 16:26:32 +0900

    Add: Song 추가, 삭제, 리스트조회, 앨범조회 - 노래 추가 - 노래 삭제 - 노래 리스트 조회 - 곡이 속한 앨범 조회

--- music_streaming/urls.py
+++ music_streaming/urls.py
@@ -1,15 +1,17 @@
 from django.urls import path, include
 from rest_framework.routers import SimpleRouter
 
-from music_streaming.views import AlbumViewSet, MusicianViewSet
+from music_streaming.views import AlbumViewSet, MusicianViewSet, SongViewSet
+
 
 app_name = 'music_streaming'
 
 router = SimpleRouter()
 
 router.register('albums', AlbumViewSet, basename='albums')
-# router.register('songs', SongViewSet, basename='songs')
 router.register('musicians', MusicianViewSet, basename='musicians')
+router.register('songs', SongViewSet, basename='songs')
+
 
 urlpatterns = [
     path('', include((router.urls, 'music_streaming'))),
