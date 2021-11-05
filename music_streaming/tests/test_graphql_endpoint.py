f62e158fa3be9238e4ca302478168f2d5e22401e
TaewooKim <embedded61@gmail.com>
2021-11-05 23:05:53 +0900

    Add: GraphQL용 TestCase 작성(AlbumTestCase, MusicianTestCase)

--- music_streaming/tests/test_graphql_endpoint.py
+++ music_streaming/tests/test_graphql_endpoint.py
@@ -1,13 +1,11 @@
-from neomodel import db, clear_neo4j_database
-from rest_framework.test import APITestCase
+from neomodel               import db, clear_neo4j_database
+from rest_framework.test    import APITestCase
 
 from music_streaming.schema import schema
 from music_streaming.models import Song, Album, Musician
 
 
 class SongTestCase(APITestCase):
-    uuid = ""
-
     def setUp(self):
         clear_neo4j_database(db)
 
@@ -76,13 +74,115 @@ class SongTestCase(APITestCase):
         return musicianList
 
 
-
-
-
-
 class AlbumTestCase(APITestCase):
-    pass
+    def setUp(self):
+        clear_neo4j_database(db)
+        
+        self.song1 = Song(name="신나는 노래").save()
+        self.song2 = Song(name="즐거운 노래").save()
+
+        self.album1 = Album(name="신나는 앨범").save()
+        self.album2 = Album(name="즐거운 앨범").save()
+        
+        self.musician1 = Musician(name="신난 가수").save()
+        self.musician2 = Musician(name="즐거운 가수").save()
+        self.musician3 = Musician(name="즐거운 가수 동생").save()
+
+        self.song1.album.connect(self.album1)
+        self.song2.album.connect(self.album2)
+            
+        self.song1.musician.connect(self.musician1)
+        self.song2.musician.connect(self.musician2)
+        self.song2.musician.connect(self.musician3)
+
+    def tearDown(self):
+        clear_neo4j_database(db)
+
+    def test_related_song_of_album(self):
+        query = f"""
+                query {{
+                    getSongOfAlbum(uuid: "{self.album1.uuid}") {{
+                        name
+                        uuid
+                    }}
+                }}
+                """
+        result = schema.execute_sync(query)
+        self.assertEqual(result.errors, None)
+
+        expected_data = [self.song1.to_dictionary()]
+        self.assertEqual(result.data['getSongOfAlbum'], expected_data)
+
+
+    def test_related_musician_of_album(self):
+        query = f"""
+                query {{
+                    getMusicianOfAlbum(uuid: "{self.album2.uuid}") {{
+                        name
+                        uuid
+                    }}
+                }}
+                """
+        result = schema.execute_sync(query)
+        self.assertEqual(result.errors, None)
+
+        expected_data = [self.musician2.to_dictionary(), self.musician3.to_dictionary()]
+        self.assertEqual(result.data['getMusicianOfAlbum'], expected_data)
 
 
 class MusicianTestCase(APITestCase):
-    pass
+    def setUp(self):
+        clear_neo4j_database(db)
+        
+        self.song1 = Song(name="신나는 노래").save()
+        self.song2 = Song(name="즐거운 노래").save()
+
+        self.album1 = Album(name="신나는 앨범").save()
+        self.album2 = Album(name="즐거운 앨범").save()
+        
+        self.musician1 = Musician(name="신난 가수").save()
+        self.musician2 = Musician(name="즐거운 가수").save()
+        self.musician3 = Musician(name="즐거운 가수 동생").save()
+
+        self.song1.album.connect(self.album1)
+        self.song2.album.connect(self.album2)
+            
+        self.song1.musician.connect(self.musician1)
+        self.song2.musician.connect(self.musician2)
+        self.song2.musician.connect(self.musician3)
+
+
+    def tearDown(self):
+        clear_neo4j_database(db)
+
+
+    def test_related_album_of_musician(self):
+        query = f"""
+                query {{
+                    getAlbumOfMusician(uuid: "{self.musician3.uuid}") {{
+                        name
+                        uuid
+                    }}
+                }}
+                """
+        result = schema.execute_sync(query)
+        self.assertEqual(result.errors, None)
+
+        expected_data = [self.album2.to_dictionary()]
+        self.assertEqual(result.data['getAlbumOfMusician'], expected_data)
+
+
+    def test_related_song_of_musician(self):
+        query = f"""
+                query {{
+                    getSongOfMusician(uuid: "{self.musician1.uuid}") {{
+                        name
+                        uuid
+                    }}
+                }}
+                """
+        result = schema.execute_sync(query)
+        self.assertEqual(result.errors, None)
+
+        expected_data = [self.song1.to_dictionary()]
+        self.assertEqual(result.data['getSongOfMusician'], expected_data)
