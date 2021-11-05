47a3fbc4bb19bfb20e23c180ca19ff4a6d9d9f3d
TaewooKim <embedded61@gmail.com>
2021-11-05 20:44:09 +0900

    Add: TestCase 작성을 위한 파일 구조 생성 - RestAPI와 GraphQL 테스트 케이스 작성 파일 분리

---
+++ music_streaming/tests/test_graphql_endpoint.py
@@ -0,0 +1,16 @@
+from rest_framework.test    import APITestCase
+
+from music_streaming.schema import schema
+from music_streaming.models import Song, Album, Musician
+
+
+class SongTestCase(APITestCase):
+    pass
+
+
+class AlbumTestCase(APITestCase):
+    pass
+
+
+class MusicianTestCase(APITestCase):
+    pass
---
+++ music_streaming/tests/test_restapi_view.py
@@ -0,0 +1,12 @@
+from rest_framework      import status
+from rest_framework.test import APITestCase
+
+from music_streaming.models import Song, Album, Musician
+
+
+class TestCase(APITestCase):
+    pass
+
+
+class TestCase(APITestCase):
+    pass
