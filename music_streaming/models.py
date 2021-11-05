26095b9396a5e047946bb0a29162b8fec4b3338d
TaewooKim <embedded61@gmail.com>
2021-11-05 14:42:50 +0900

    Add: GraphQL 사용을 위한 기본적인 구조 생성 - graphql url 설정 - 각 모델을 graphql로 사용하기 위한 type 생성 - 기본적인 schema 생성

--- music_streaming/models.py
+++ music_streaming/models.py
@@ -1,3 +1,5 @@
+import json
+
 from neomodel import (
                 StructuredNode, StringProperty, RelationshipTo,
                  One, RelationshipFrom, OneOrMore
@@ -9,12 +11,21 @@ class Song(StructuredNode):
     album    = RelationshipTo('Album', 'belongsto', cardinality=One)
     musician = RelationshipTo('Musician', 'writtenby', cardinality=OneOrMore)
 
+    def to_dictionary(self):
+        return json.loads(json.dumps(self.__properties__, ensure_ascii=False))
+
 
 class Album(StructuredNode):
     name = StringProperty(unique_index=True)
     song = RelationshipFrom('Song', 'belongsto', cardinality=OneOrMore)
 
+    def to_dictionary(self):
+        return json.loads(json.dumps(self.__properties__, ensure_ascii=False))
+
 
 class Musician(StructuredNode):
     name = StringProperty(unique_index=True)
     song = RelationshipFrom('Song', 'writtenby', cardinality=OneOrMore)
+
+    def to_dictionary(self):
+        return json.loads(json.dumps(self.__properties__, ensure_ascii=False))
